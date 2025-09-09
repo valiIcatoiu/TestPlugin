2f8d9c8dc0becb7aee940e52dfddef9a7e4746ea
valiIcatoiu <37401649+valiIcatoiu@users.noreply.github.com>
2025-09-09 15:23:26 +0300

    Update ExternalPlugin_v1_1.js

--- ExternalPlugin_v1_1.js
+++ ExternalPlugin_v1_1.js
@@ -4,7 +4,6 @@ function ExternalDemoWidget(props) {
   const settings = props?.settings || {};
   return React.createElement(
     "div",
-    { style: { border: "1px dashed #16a34a", padding: 10, borderRadius: 8, fontFamily: "sans-serif" } },
     [
       React.createElement("strong", { key: "t" }, "ExternalDemoWidget"),
       React.createElement("div", { key: "s", style: { fontSize: 12, marginTop: 6 } }, "settings: " + JSON.stringify(settings))
